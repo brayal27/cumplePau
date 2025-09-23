31f7b392e5681d546049d2d7c1d14a380df48e8a
brayal27 <brayal@uoc.edu>
2025-09-23 21:35:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hoy = new Date();
+  const diaHoy = hoy.getDate();       // 1–31
+  const mesHoy = hoy.getMonth() + 1;  // 1–12
+
+  document.querySelectorAll("td.bloqueado").forEach((td) => {
+    const numeroDia = parseInt(td.dataset.dia);
+    const numeroMes = parseInt(td.dataset.mes);
+    const link = td.querySelector("a");
+
+    if (numeroDia <= diaHoy && numeroMes === mesHoy) {
+      td.classList.remove("bloqueado");
+      if (link) link.style.pointerEvents = "auto";
+    } else {
+      if (link) link.style.pointerEvents = "none";
+    }
+  });
+});
