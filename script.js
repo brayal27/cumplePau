1ceb5df94871c460d008826b2ccd3b3f3be2760c
brayal27 <brayal@uoc.edu>
2025-10-06 21:42:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const numeroMes = parseInt(td.dataset.mes);
     const link = td.querySelector("a");
 
-    if (numeroDia <= diaHoy && numeroMes === mesHoy) {
+    // Desbloquear si:
+    // - es septiembre (9) y el día está entre 26 y 30, o
+    // - es octubre (10) y el día es menor o igual al actual
+    if (
+      (numeroMes === 9 && numeroDia >= 26 && numeroDia <= 30) ||
+      (numeroMes === 10 && numeroDia <= diaHoy)
+    ) {
       td.classList.remove("bloqueado");
       if (link) link.style.pointerEvents = "auto";
     } else {
